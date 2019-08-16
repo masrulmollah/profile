8c5df022076b454bd02ffad97f51ac474d913233
masrulmollah <54163010+masrulmollah@users.noreply.github.com>
2019-08-16 23:12:53 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-document.write("MdMasrulMollah");
-document.write("Muhammad Ibne Masrul");
+<!doctype html>
+  <html>
+  <title> variable , arrays </title>
+<script>
+  var car = "honda";
+  </script>
+</head>
+<body>
+  <script>
+  document.write("anything");
+  </script>
+  </body>
+</html>
 
