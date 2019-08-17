6e441cc65b20aca70a307fe6523004df6bc940d3
masrulmollah <54163010+masrulmollah@users.noreply.github.com>
2019-08-17 10:01:10 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+	<head>
+		<title> Variables , Arrays </title>
+		<script>
+			var car = "honda";
+		</script>
+	</head>
+	<body>
+		<script>
+			document.write("anything");
+		</script>
+	</body>
+</html>
+
+
